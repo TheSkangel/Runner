cb7747f1855c8b2aaf9abb5ab19814ef46992003
TheSkangel <chr.jaros@hotmail.de>
2017-03-11 20:37:11 -0500

    Update Nr ???
    Don't know never work in master branch

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public Rigidbody rb;
+    public Animator anim;
     public float speed;
     public float force;
+    public KeyCode player1HButton;
+    public KeyCode player1JButton;
 
     private Vector3 PlayerPosition;
+    private KeyCode[] buttonsp1h;
+    private KeyCode[] buttonsp1j;
 
     bool grounded = false;
     bool playerHit = false;
@@ -17,15 +22,33 @@ public class PlayerMovement : MonoBehaviour {
     private float originalTime;
     private float originalSpeed;
 
-    void Start () {
+    public float ButtonTimer;
+    private float originalButtonTimer;
+
+    void Awake()
+    {
+        anim.GetComponent<Animator>();
         rb.GetComponent<Rigidbody>();
         originalSpeed = speed;
         originalTime = timer;
-	}
-	
-	void FixedUpdate () {
+        buttonsp1j = new KeyCode[] { KeyCode.Q, KeyCode.W, KeyCode.E, KeyCode.R, KeyCode.A, KeyCode.S, };
+        buttonsp1h = new KeyCode[] { KeyCode.D, KeyCode.F, KeyCode.V, KeyCode.C, KeyCode.X, KeyCode.Z };
+        originalButtonTimer = ButtonTimer;
+    }
 
-        Move();
+    void Start () {
+        player1HButton = buttonsp1h[Random.Range(0, buttonsp1h.Length)];
+        player1JButton = buttonsp1j[Random.Range(0, buttonsp1j.Length)];
+    }
+
+    void Update()
+    {
+        resetButtons();
+    }
+
+    void FixedUpdate () {
+
+        movePlayer1();
         SlowDown();
 	}
 
@@ -44,21 +67,50 @@ public class PlayerMovement : MonoBehaviour {
         }
     }
 
-    public void Move()
+    void resetButtons()
     {
-        if (Input.GetButtonDown("P1Horizontal"))
+        ButtonTimer -= Time.deltaTime;
+        if(ButtonTimer <= 0)
+        {
+            //if (player1HButton.CompareTo(player1JButton) == 0 && player1HButton.CompareTo(player2HButton) == 0 && player1HButton.CompareTo(player2JButton) == 0)
+            player1HButton = buttonsp1h[Random.Range(0, buttonsp1h.Length)];
+            player1JButton = buttonsp1j[Random.Range(0, buttonsp1j.Length)];
+            ButtonTimer = originalButtonTimer;
+        }
+    }
+
+    public void movePlayer1()
+    {
+        if (Input.GetKeyDown(player1HButton))
         {
             PlayerPosition = rb.transform.position;
             PlayerPosition = PlayerPosition + new Vector3 (1f,0f,0f) * speed * Time.deltaTime;
             rb.MovePosition(PlayerPosition);
+            anim.Play("Armature|Run_blocking");
         }
 
-        if (Input.GetButtonDown("P1Jump") && grounded == true)
+        if (Input.GetKeyDown(player1JButton) && grounded == true)
         {
             rb.AddForce(new Vector3(0f, force, 0f), ForceMode.Impulse);
             grounded = false;
         }
     }
+
+    //public void Move()
+    //{
+    //    if (Input.GetButtonDown("P1Horizontal"))
+    //    {
+    //        PlayerPosition = rb.transform.position;
+    //        PlayerPosition = PlayerPosition + new Vector3 (1f,0f,0f) * speed * Time.deltaTime;
+    //        rb.MovePosition(PlayerPosition);
+    //    }
+
+    //    if (Input.GetButtonDown("P1Jump") && grounded == true)
+    //    {
+    //        rb.AddForce(new Vector3(0f, force, 0f), ForceMode.Impulse);
+    //        grounded = false;
+    //    }
+    //}
 
     void OnTriggerEnter(Collider coll)
     {
