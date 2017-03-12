3fc7a9a7815361dc21be4fbc5f6ccabd17b923c8
Jimjimbo <data.jimmy.cao@gmail.com>
2017-03-11 22:22:45 -0500

    Add files via upload

---
+++ Assets (menu)/Scripts/ButtonManagement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonManagement : MonoBehaviour
+{
+	public void StartGameBTN(string StartGameRace)
+	{
+		SceneManager.LoadScene (StartGameRace);
+	}
+
+	public void ExitButton()
+	{
+		Application.Quit ();
+	}
+}
---
+++ Assets (menu)/Scripts/NextScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+	public Canvas startup;
+
+	// Use this for initialization
+	void Start() {
+		startup = startup.GetComponent<Canvas>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetKey(KeyCode.A)&& Input.GetKey(KeyCode.L))
+		{ SceneManager.LoadScene("1");
+		}
+	}
+	public void ExitButton() {
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{ Application.Quit ();
+		}
+	}
+}
+
+	
